fd17af60bee253aa39d5dccb69ec442fc8da54b2
IlianYanev <iyanev66@gmail.com>
2025-10-24 15:58:38 +0300

    Fixing some visual bugs

--- src/main/java/com/logistics/company/Controller/MainController.java
+++ src/main/java/com/logistics/company/Controller/MainController.java
@@ -1,21 +1,12 @@
-package com.logistics.company.Controller;// package com.logistics.company.controller; // Уверете се, че пакетът е правилен
-
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.GetMapping;
-
+package com.logistics.company.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // <-- Това е КЛЮЧОВОТО!
+@Controller
 public class MainController {
 
-    /**
-     * Този метод казва на Spring:
-     * Когато някой отвори http://localhost:8080/
-     * върни му файла /src/main/resources/templates/index.html
-     */
     @GetMapping("/")
     public String showHomePage() {
-        return "index"; // <-- Трябва да съвпада с името на файла (index.html)
+        return "index";
     }
 }
